ef00defdb42e329ad85d357ed555e25c74844133
Alia Alsuwaidi <aalsuwai@lab1r1s3.42abudhabi.ae>
2022-09-01 16:53:20 +0400

    start creating the tester

---
+++ test.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+blue=`tput setab 219`
+red=`tput setaf 1`
+reset=`tput sgr0`
+
+# VectorTest() {
+# 	# make a file inside the test file and name it "vectorTest"
+# 	# run the .cpp test for vector then compare files in the directory
+# 	# put the result of diff in a file and output ✅ or ❌ depending on the result
+# }
+
+# MapTest() {
+# 	# make a file inside the test file and name it "mapTest"
+# 	# run the .cpp test for vector then compare files in the directory
+# 	# put the result of diff in a file and output ✅ or ❌ depending on the result
+# }
+
+# StackTest() {
+# 	# make a file inside the test file and name it "stackTest"
+# 	# run the .cpp test for vector then compare files in the directory
+# 	# put the result of diff in a file and output ✅ or ❌ depending on the result
+# }
+
+# AllTset() {
+# 	VectorTest()
+# 	MapTest()
+# 	StackTest()
+# }
+
+echo "${red}Select a number:${reset}"
+echo "${blue}1) vector${reset}"
+echo "2) map"
+echo "3) stack"
+echo "4) all"
+read test_type
+
+if [ $test_type == 1 ]
+then
+	clear
+	# VectorTest()
+	echo "howowowo VECTOR"
+
+elif [ $test_type == 2 ]
+then
+	clear
+	# MapTest()
+	echo "howowowo MAP"
+
+elif [ $test_type == 3 ]
+then
+	clear
+	# StackTest()
+	echo "howowowo STACK"
+
+elif [ $test_type == 4 ]
+then
+	clear
+	# AllTset()
+	echo "howowowo ALLLLLLLLLL"
+
+else
+	echo "TF?"
+fi
